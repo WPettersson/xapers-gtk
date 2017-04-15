c252078164944740067e3900145cb50fc1d2e85c
William Pettersson <william.pettersson@gmail.com>
2017-04-15 12:14:44 +1000

    Initial commit.
    Can search and display search results at least. Things to still do
    follow.
    
    Fix resizing: I want to make sure the user doesn't resize things off the
    end. Once that's fixed, I can set suitable sizeHints for the columns and
    allow auto-resizing as the window changes.
    
    View PDF: Rather than a "Yes", I want a clickable link for PDFs.
    Probably need to get the source of the entry to find the correct
    directory from the relative one.
    
    Edit window: Some way of editing what's in the database, i.e. adding
    tags
    
    Adding new entries: Be nice to be able to add new entries. Maybe even
    from a .bib file
    
    User configuration: DB location, window size, etc.

---
+++ xapers-gtk.py
@@ -0,0 +1,217 @@
+#!/usr/bin/env python2
+
+# Load config
+
+
+# Searching
+# tag:string
+# show only things with "string" inside tag
+# support and/or keywords, and brackets?
+
+
+# Layout
+#
+# Searchbox
+# ---------
+# Entry
+# Entry
+
+# Entry = Title, Authors, Year, PDF-link
+
+import sys
+import xapers
+import gettext
+from PyQt5.QtWidgets import (QApplication, QWidget, QLineEdit, QBoxLayout,
+                             QPushButton, QTableView, QStackedWidget, QLabel,
+                             QHeaderView, QAbstractItemView, QShortcut)
+from PyQt5.QtCore import (Qt, QCoreApplication, QAbstractItemModel,
+                          QModelIndex, QSize)
+from PyQt5.QtGui import QKeySequence, QFontMetrics, QFont
+
+gettext.bindtextdomain('xapers-gtk', '/path/to/my/language/directory')
+gettext.textdomain('xapers-gtk')
+_ = gettext.gettext
+
+
+db = xapers.Database('~/.xapers/docs')
+
+CONSTS = {}
+CONSTS['cols'] = 4
+
+HEADINGS = [_("Title"), _("Author(s)"), _("Year"), _("PDF")]
+
+
+class ResultsWidget(QStackedWidget):
+    def __init__(self):
+        super(ResultsWidget, self).__init__()
+        noResults = QWidget()
+        noResultsLayout = QBoxLayout(QBoxLayout.TopToBottom)
+        noResultsLabel = QLabel()
+        noResultsLabel.setText("No results found")
+        noResultsLayout.addWidget(noResultsLabel)
+        noResults.setLayout(noResultsLayout)
+        self.addWidget(noResults)
+
+        self.results = PapersTable()
+        self.addWidget(self.results)
+
+    def doSearch(self, searchString=""):
+        docs = db.search(searchString)
+        if len(docs) == 0:
+            self.setCurrentIndex(0)
+        else:
+            self.results.addResults(docs)
+            self.setCurrentIndex(1)
+
+
+class PapersTable(QTableView):
+    def __init__(self):
+        super(PapersTable, self).__init__()
+        self.model = PapersModel()
+        self.setModel(self.model)
+        self.setSortingEnabled(True)
+        self.horizontalHeader().show()
+        self.horizontalHeader().setSectionsMovable(True)
+        self.horizontalHeader().setStretchLastSection(True)
+        self.horizontalHeader().setSectionResizeMode(QHeaderView.Interactive)
+        self.horizontalHeader().resizeSections(QHeaderView.ResizeToContents)
+        self.setSelectionBehavior(QAbstractItemView.SelectRows)
+        self.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
+
+    def addResults(self, docs=[]):
+        self.model.setDoc(docs)
+
+
+class PapersModel(QAbstractItemModel):
+    def __init__(self):
+        super(PapersModel, self).__init__()
+        metric = QFontMetrics(QFont())
+        # TODO Could probably be done better
+        self.yearWidth = metric.width("8888") + 10
+        self.docs = []
+
+    def setDoc(self, docs):
+        self.beginResetModel()
+        self.docs = list(docs)
+        self.endResetModel()
+
+    def index(self, row, column, parent):
+        return QAbstractItemModel.createIndex(self, row, column,
+                                              CONSTS['cols'] * row + column)
+
+    def parent(self, index):
+        return QModelIndex()
+
+    def columnCount(self, unused):
+        return CONSTS['cols']
+
+    def rowCount(self, unused):
+        return len(self.docs)
+
+    def data(self, index, role):
+        if role == Qt.SizeHintRole:
+            if index.column() == 2:
+                return QSize(25, 15)
+        if role != Qt.DisplayRole:
+            return None
+        doc = self.docs[index.row()]
+        if index.column() == 0:
+            return str(doc.get_title())
+        elif index.column() == 1:
+            auth = doc.get_authors()
+            if len(auth) > 3:
+                return ", ".join(auth[0:2]) + " et al."
+            else:
+                return ", ".join(auth)
+        elif index.column() == 2:
+            return doc.get_year()
+        elif index.column() == 3:
+            f = doc.get_files()
+            if len(f) > 0:
+                # s = "%s" % (f[0])
+                return "Yes"
+            return None
+        return None
+
+    def sort(self, which, order):
+        reverse = (order == Qt.DescendingOrder)
+        if which == 0:  # Title
+            self.layoutAboutToBeChanged.emit()
+            self.docs.sort(key=lambda d: d.get_title(), reverse=reverse)
+            self.layoutChanged.emit()
+        elif which == 1:  # Authors
+            self.layoutAboutToBeChanged.emit()
+            self.docs.sort(key=lambda d: d.get_authors(), reverse=reverse)
+            self.layoutChanged.emit()
+        elif which == 2:  # Years
+            self.layoutAboutToBeChanged.emit()
+            self.docs.sort(key=lambda d: d.get_year(), reverse=reverse)
+            self.layoutChanged.emit()
+
+    def headerData(self, section, orientation, role):
+        if orientation != Qt.Horizontal:
+            return None
+        if role == Qt.SizeHintRole:
+            if section == 0:  # Title
+                return QSize(500, 15)
+            if section == 2:
+                return QSize(self.yearWidth, 15)
+        if role != Qt.DisplayRole:
+            return None
+        if section < len(HEADINGS):
+            return HEADINGS[section]
+        return None
+
+
+class DocWindow(QWidget):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        self.makeUI()
+
+    def makeUI(self):
+        self.resize(400, 300)
+        self.setWindowTitle("xapers-gtk: Document editor")
+
+
+class MainWindow(QWidget):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        self.makeUI()
+
+    def startSearch(self):
+        self.resultWidget.doSearch(self.searchLine.text())
+
+    def makeUI(self):
+        self.resize(800, 300)
+        self.setWindowTitle("xapers-gtk")
+        layout = QBoxLayout(QBoxLayout.TopToBottom)
+
+        searchLayout = QBoxLayout(QBoxLayout.LeftToRight)
+        self.searchLine = QLineEdit()
+        searchButton = QPushButton("Search")
+        searchButton.clicked.connect(self.startSearch)
+        self.searchLine.returnPressed.connect(self.startSearch)
+        searchLayout.addWidget(self.searchLine)
+        searchLayout.addWidget(searchButton)
+        layout.addLayout(searchLayout)
+
+        self.resultWidget = ResultsWidget()
+        layout.addWidget(self.resultWidget)
+
+        self.setLayout(layout)
+
+        esc = QShortcut(QKeySequence("Esc"), self)
+        ctrlq = QShortcut(QKeySequence(Qt.CTRL + Qt.Key_Q), self)
+        esc.activated.connect(QCoreApplication.instance().quit)
+        ctrlq.activated.connect(QCoreApplication.instance().quit)
+        self.show()
+
+
+def main():
+    app = QApplication(sys.argv)
+    m = MainWindow()
+    assert m
+    sys.exit(app.exec_())
+
+if __name__ == "__main__":
+    main()
